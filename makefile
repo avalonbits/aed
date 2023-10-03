b6c3df0fc9ffe65b252ece2f6664021e319a58fa
Igor Cananea <icc@avalonbits.com>
2023-10-03 19:05:32 -0400

    Use c99 standard and show it works with a for loop.

--- makefile
+++ makefile
@@ -8,6 +8,7 @@ DESCRIPTION = "Agoe text editor."
 COMPRESSED = NO
 ARCHIVED = NO
 INIT_LOC = 0B0000
+BSSHEAP_LOW = 040000
 
 CFLAGS = -Wall -Wextra -Oz
 CXXFLAGS = -Wall -Wextra -Oz
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, world!");
+    for (int i = 0; i < 10; i++) {
+        printf("Hello, world!\n");
+    }
     return 0;
 }
